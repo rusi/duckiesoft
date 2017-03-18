2578665876489457fe3f8cd406f7ed32ca8e1ff9
Ruslan Hristov <rusi@me.com>
2017-03-18 13:26:59 -0400

    updated ROS Dev container

--- common.sh
+++ common.sh
@@ -4,5 +4,6 @@ DUCKIEBOT_HOST=duckiebot.local
 DUCKIEBOT_IP=$(ping -c 1 $DUCKIEBOT_HOST | awk -F'[()]' 'NR==1{print $2}')
 
 HOST_IP=$(ping -c 1 -R $DUCKIEBOT_HOST | awk 'NR==5{print $1}' | sed 's/ //')
+#HOST_IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
 
 RUN_CMD=" run --env ROS_MASTER_URI=http://$DUCKIEBOT_IP:11311 "
--- rosdev.sh
+++ rosdev.sh
@@ -4,7 +4,17 @@ DIR=$( cd "$(dirname "$0")" ; pwd -P )
 
 source $DIR/common.sh
 
+xhost + $HOST_IP
+
+# Comment out the next line if you want to use Duckiebot's ROS Master
+RUN_CMD="run --env ROS_MASTER_URI=http://rosmaster:11311"
+
 docker $RUN_CMD -it --rm \
+	-e DISPLAY=$HOST_IP:0 \
+	-v /tmp/.X11-unix:/tmp/.X11-unix \
 	-v ~/.ros:/root/.ros/ \
 	-v ${PWD}:/workspace \
+	--net duckienet \
 	rosdev $@
+
+	#--name rosmaster \
