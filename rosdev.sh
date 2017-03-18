51ee776468925398c843ef54140a004f51886a5b
Ruslan Hristov <rusi@me.com>
2017-03-18 14:29:56 -0400

    auto-starting ROS Master container if it is not running (locally)

--- rosdev.sh
+++ rosdev.sh
@@ -6,15 +6,24 @@ source $DIR/common.sh
 
 xhost + $HOST_IP
 
+CONTAINER="rosdev"
+RUN_FLAGS="-it --rm"
+
+docker ps | grep -q rosdev
+if [ $? -ne 0 ]; then
+	CONTAINER="--name rosmaster rosdev roscore"
+	RUN_FLAGS="-d"
+	echo "ROS Master NOT running... Starting..."
+fi
+
 # Comment out the next line if you want to use Duckiebot's ROS Master
 RUN_CMD="run --env ROS_MASTER_URI=http://rosmaster:11311"
 
-docker $RUN_CMD -it --rm \
+
+docker $RUN_CMD $RUN_FLAGS \
 	-e DISPLAY=$HOST_IP:0 \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
 	-v ~/.ros:/root/.ros/ \
 	-v ${PWD}:/workspace \
 	--net duckienet \
-	rosdev $@
-
-	#--name rosmaster \
+	$CONTAINER $@
